3037ea571d0e72eec87d9423f6911842d153274c
Judicaël Grasset <judicael.grasset@meteo.fr>
2022-11-15 16:52:07 +0000

    add global var for code which doesnt use types

--- src/modd_convpar.F90
+++ src/modd_convpar.F90
@@ -65,5 +65,37 @@ REAL :: XMELDPTH    ! layer (Pa) through which precipitation melt is
                     ! allowed below  melting level
 REAL :: XUVDP       ! constant for pressure perturb in momentum transport
 END TYPE CONVPAR_T
+
+!Keep global variables for parts of the code not ported to the type yet
+REAL :: XA25        ! 25 km x 25 km reference grid area
+!
+REAL :: XCRAD       ! cloud radius
+REAL :: XCDEPTH     ! minimum necessary cloud depth
+REAL :: XENTR       ! entrainment constant (m/Pa) = 0.2 (m)
+!
+REAL :: XZLCL       ! maximum allowed allowed height
+                    ! difference between departure level and surface
+REAL :: XZPBL       ! minimum mixed layer depth to sustain convection
+REAL :: XWTRIG      ! constant in vertical velocity trigger
+!
+!
+REAL :: XNHGAM      ! accounts for non-hydrost. pressure
+                    ! in buoyancy term of w equation
+                    ! = 2 / (1+gamma)
+REAL :: XTFRZ1      ! begin of freezing interval
+REAL :: XTFRZ2      ! end of freezing interval
+!
+REAL :: XRHDBC      ! relative humidity below cloud in downdraft
+!
+REAL :: XRCONV      ! constant in precipitation conversion
+REAL :: XSTABT      ! factor to assure stability in  fractional time
+                    ! integration, routine CONVECT_CLOSURE
+REAL :: XSTABC      ! factor to assure stability in CAPE adjustment,
+                    !  routine CONVECT_CLOSURE
+REAL :: XUSRDPTH    ! pressure thickness used to compute updraft
+                    ! moisture supply rate for downdraft
+REAL :: XMELDPTH    ! layer (Pa) through which precipitation melt is
+                    ! allowed below  melting level
+REAL :: XUVDP       ! constant for pressure perturb in momentum transport
 !
 END MODULE MODD_CONVPAR
--- src/modd_convparext.F90
+++ src/modd_convparext.F90
@@ -10,5 +10,10 @@ INTEGER :: JCVEXB ! start vertical computations at
 INTEGER :: JCVEXT ! limit vertical computations to
                         ! KLEV - JCVEXT = KLEV - ( KTDIA - 1 )
 END TYPE CONVPAREXT
+!Keep global variables for parts of the code not ported to the type ye
+INTEGER :: JCVEXB ! start vertical computations at
+                        ! 1 + JCVEXB = 1 + ( KBDIA - 1 )
+INTEGER :: JCVEXT ! limit vertical computations to
+                        ! KLEV - JCVEXT = KLEV - ( KTDIA - 1 )
 !
 END MODULE MODD_CONVPAREXT
